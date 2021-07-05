57a93fa3b1f38e81b461a912d507035b108c9b2c
John Bryan Delda <mightypirates174@gmail.com>
2021-07-05 16:57:06 +0800

    Exercise 1.2 - courseinformation, step2

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,35 +2,34 @@ import React from "react";
 
 const App = () => {
 	const course = "Half Stack application development";
-
 	const part1 = "Fundamentals of React";
 	const exercises1 = 10;
 	const part2 = "Using props to pass data";
 	const exercises2 = 7;
 	const part3 = "State of a component";
 	const exercises3 = 14;
+
 	const Header = ({ course }) => <h1>{course}</h1>;
 	const Content = () => (
 		<>
-			<p>
-				{part1} {exercises1}
-			</p>
-			<p>
-				{part2} {exercises2}
-			</p>
-			<p>
-				{part3} {exercises3}
-			</p>
+			<Part name={part1} number={exercises1} />
+			<Part name={part2} number={exercises2} />
+			<Part name={part3} number={exercises3} />
 		</>
 	);
-  const Total=()=>(
-    <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-  )
+	const Part = ({ name, number }) => (
+		<p>
+			{name} {number}
+		</p>
+	);
+	const Total = () => (
+		<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+	);
 	return (
 		<div>
 			<Header course={course} />
-      <Content/>
-			<Total/>
+			<Content />
+			<Total />
 		</div>
 	);
 };
