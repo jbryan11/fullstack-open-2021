ac52062d1864f254c431b594ee1e3b3e45af8af7
John Bryan Delda <mightypirates174@gmail.com>
2021-07-05 17:20:44 +0800

    Exercise 1.3 - courseinformation, step3

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -2,21 +2,27 @@ import React from "react";
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
+	const part1 = {
+		name: "Fundamentals of React",
+		exercises: 10,
+	};
+	const part2 = {
+		name: "Using props to pass data",
+		exercises: 7,
+	};
+	const part3 = {
+		name: "State of a component",
+		exercises: 14,
+	};
 
 	const Header = (props) => {
-    console.log(props)
-    return<h1>{props.course}</h1>};
+		return <h1>{props.course}</h1>;
+	};
 	const Content = () => (
 		<>
-			<Part name={part1} number={exercises1} />
-			<Part name={part2} number={exercises2} />
-			<Part name={part3} number={exercises3} />
+			<Part name={part1} number={part1.exercises1} />
+			<Part name={part2} number={part2.exercises2} />
+			<Part name={part3} number={part3.exercises3} />
 		</>
 	);
 	const Part = ({ name, number }) => (
@@ -25,7 +31,10 @@ const App = () => {
 		</p>
 	);
 	const Total = () => (
-		<p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+		<p>
+			Number of exercises{" "}
+			{part1.exercises1 + part2.exercises2 + part3.exercises3}
+		</p>
 	);
 	return (
 		<div>
