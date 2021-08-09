d117f9095d73274b1715ab018a5eb285d98d3bcc
peekabo0saku <7879929+jbryan11@users.noreply.github.com>
2021-08-09 13:24:38 +0800

    Part 2 : courseinfo, exercise 2.1

---
+++ part2/courseinfo/components/Content.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+const Content = ({ course }) => {
+    return (
+      <div>
+        <Part part={course.parts[0]} />
+        <Part part={course.parts[1]} />
+        <Part part={course.parts[2]} />
+      </div>
+    )
+  }
+  export default Content
