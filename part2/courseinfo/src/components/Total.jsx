7b0852d2040c6bda197e96301d6d9a70590c6cd6
peekabo0saku <7879929+jbryan11@users.noreply.github.com>
2021-08-09 19:50:00 +0800

    Part 2 : courseinfo, exercise 2.3

--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -1,9 +1,14 @@
-
 const Total = ({ course }) => {
-    const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
-    return(
-      <p><strong>Number of exercises {sum}</strong></p>
-    ) 
-  }
+	let initValue = 0;
+	const sum = course.parts.reduce(
+		(prev, current) => prev + current.exercises,
+		initValue
+	);
+	return (
+		<p>
+			<strong>Number of exercises {sum}</strong>
+		</p>
+	);
+};
 
-  export default Total
+export default Total;
