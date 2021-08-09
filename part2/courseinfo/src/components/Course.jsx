0ff5265562c64fe65ebcc38a9c9eac047f89c94d
peekabo0saku <7879929+jbryan11@users.noreply.github.com>
2021-08-09 19:34:42 +0800

    Part 2 : courseinfo, exercise 2.2

--- part2/courseinfo/src/components/Course.jsx
+++ part2/courseinfo/src/components/Course.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Header from './Header.jsx'
 import Content from "./Content.jsx";
+import Total from './Total.jsx'
 
 const Course = ({ course }) => {
 	return (
 		<React.Fragment>
 			<Header course={course} />
 			<Content course={course} />
+			<Total course={course}/>
 		</React.Fragment>
 	);
 };
--- part2/courseinfo/src/components/Total.jsx
+++ part2/courseinfo/src/components/Total.jsx
@@ -2,7 +2,7 @@
 const Total = ({ course }) => {
     const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
     return(
-      <p>Number of exercises {sum}</p>
+      <p><strong>Number of exercises {sum}</strong></p>
     ) 
   }
 
