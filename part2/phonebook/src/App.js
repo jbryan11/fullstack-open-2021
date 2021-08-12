a6d1846bd5f9eb7cac71b394396de2ef843d8043
peekabo0saku <7879929+jbryan11@users.noreply.github.com>
2021-08-12 18:58:49 +0800

    Part 2 : Phonebook, exercise 2.7-2.10

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,33 +1,51 @@
 import React, { useState } from "react";
-
+import Filter from "./components/Filter";
+import InputForm from "./components/InputForm";
+import Input from "./components/Input";
+import Persons from "./components/Persons";
 const App = () => {
-	const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+	const [persons, setPersons] = useState([
+		{ name: "Arto Hellas", phone: "034-123456" },
+		{ name: "Ada Lovelace", phone: "39-44-5323523" },
+		{ name: "Dan Abramov", phone: "12-43-234345" },
+		{ name: "Mary Poppendieck", phone: "39-23-6423122" },
+	]);
 	const [newName, setNewName] = useState("");
-
+	const [newPhone, setNewPhone] = useState("");
+	const [filterInput, setFilterInput] = useState("");
 	const handleInputName = (event) => {
 		setNewName(event.target.value);
 	};
+	const handleInputPhone = (event) => {
+		setNewPhone(event.target.value);
+	};
+	const handleFilterInput = (event) => {
+		setFilterInput(event.target.value);
+	};
 	const addNewName = (event) => {
 		event.preventDefault();
-		const newNameObj = { name: newName };
-		setPersons([newNameObj, ...persons]);
+		setPersons([{ name: newName, phone: newPhone }, ...persons]);
+		alert(`${newName} has already been added to phonebook`);
 		setNewName("");
+		setNewPhone("");
 	};
+	const fitlerResult = !filterInput
+		? persons
+		: persons.filter((person) =>
+				person.name.toLowerCase().includes(filterInput.toLocaleLowerCase())
+		  );
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
-			<form onSubmit={addNewName}>
-				<div>
-					name: <input onChange={handleInputName} value={newName} />
-				</div>
-				<div>
-					<button type="submit">add</button>
-				</div>
-			</form>
+			<Filter handler={handleFilterInput} value={filterInput} />
+			<h2>Add new Phone</h2>
+			<InputForm formHandler={addNewName}>
+				<Input handler={handleInputName} value={newName}>Name</Input>
+				<Input handler={handleInputPhone} value={newPhone}>Phone</Input>
+			</InputForm>
 			<h2>Numbers</h2>
-			{persons.map((person) => (
-				<p key={person.name}>{person.name}</p>
-			))}
+			<Persons list={fitlerResult}/>
 		</div>
 	);
 };
