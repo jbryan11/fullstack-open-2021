b653b324414f740ccc78c5dad526cecae6ad3fc9
peekabo0saku <7879929+jbryan11@users.noreply.github.com>
2021-09-26 20:47:29 +0800

    Part 4 : Blog List , exercise 4.3-4.7

---
+++ part4/blog-list/utils/Tests/data.js
@@ -0,0 +1,47 @@
+module.exports = [
+    {
+        _id: '5a422aa71b54a676234d17f8',
+        title: 'Go To Statement Considered Harmful',
+        author: 'Edsger W. Dijkstra',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 6,
+        blogs: 65,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17f9',
+        title: 'Just some Good Ol Pal',
+        author: 'Lemeur Quaksiere',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 23,
+        blogs: 15,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17g0',
+        title: 'The new Van Gogh of the Era of the Industrialization',
+        author: 'Lany Shalaman',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 123,
+        blogs: 23,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17g1',
+        title: 'The Older days of the Ancient Civilization',
+        author: 'Pedro Salazar',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 78,
+        blogs: 538,
+        __v: 0
+    },
+    {
+        _id: '5a422aa71b54a676234d17g2',
+        title: 'Luckluster of Shenanigans',
+        author: 'Saezama Wakatatsu',
+        url: 'http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html',
+        likes: 56,
+        blogs: 123,
+        __v: 0
+    }
+]
